cad48a8657fb07f5ccee2d8476bbe9f1014ed04d
herbertdesousa <sousaherbert138@gmail.com>
2022-11-04 18:22:33 -0300

    update: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,9 @@ const Document: NextPage = () => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:wght@400;700&display=swap"
           rel="stylesheet"
         />
+
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon/favicon.png" />
       </Head>
       <body>
         <Main />
